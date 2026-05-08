e9959d987f5c194288396209b9b34caaa8800d31
amruthaklakshman29-sudo <amruthaklakshman29@gmail.com>
2026-05-08 14:46:14 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+from audio_recorder_streamlit import audio_recorder
+from deep_translator import GoogleTranslator
+from gtts import gTTS
+import speech_recognition as sr
+from io import BytesIO
+
+# Get a list of supported languages for the dropdown
+langs_dict = GoogleTranslator().get_supported_languages(as_dict=True)
+
+def main():
+    st.image("PragyanAI_Transperent.png")
+    st.title(" PragyanAI - VVIET Workshop: Audio Hub")
